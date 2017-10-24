fb09bd0f3f6a2a51a149adf42dd1442ba02f4b23
Akash <akashkrish.94@gmail.com>
2017-10-24 17:56:31 +0530

    Second commit

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ var app = express();
 hbs.registerPartials(__dirname + '/views/partials');
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
+
+
 
 app.use((req, res,next) => {
 	var now = new Date().toString();
@@ -22,6 +23,12 @@ app.use((req, res,next) => {
 hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
 hbs.registerHelper('screamIt', (text) => text.toUpperCase());
 
+// IF Maintainence 
+// app.use((req, res, next) => {
+// 	res.render('maintainence.hbs');
+// })
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
 	res.render('homepage.hbs', {
